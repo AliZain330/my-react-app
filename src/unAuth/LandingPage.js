be23179d9c105bc661d5d5c00625d52a163c5f9b
ZAIN Ali <alizainanwar468@gmail.com>
2026-01-02 01:28:23 +0500

    intro

--- src/unAuth/LandingPage.js
+++ src/unAuth/LandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './LandingPage.css';
 import Navbar from './components/Navbar';
+import Intro from './components/intro';
 import Bumpups from './components/Bumpups';
 import Timestamp from './components/Timestamp';
 import Footer from './components/Footer';
@@ -10,7 +11,7 @@ function LandingPage() {
     <div className="landing-page">
       <Navbar />
       <div className="landing-content">
-        <h1 className="landing-title">Hello what's up guys</h1>
+        <Intro />
         <Timestamp />
         <Bumpups />
       </div>
@@ -20,4 +21,3 @@ function LandingPage() {
 }
 
 export default LandingPage;
-
